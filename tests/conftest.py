86faac2ed6a6a9c7b30e56e3afd07bf003d8c4c7
Deseteral <deseteral@gmail.com>
2026-01-25 15:48:00 +0100

    Terminal output testing

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+import pyte
+import pytest
+
+
+@pytest.fixture
+def terminal(capsys):
+    class VirtualTerminal:
+        def __init__(self):
+            self.screen = pyte.Screen(8000, 240)
+            self.stream = pyte.Stream(self.screen)
+
+        def render(self):
+            self.stream.feed(capsys.readouterr().out)
+
+        @property
+        def lines(self):
+            return [
+                line.strip()
+                for line in "\n".join(self.screen.display).strip().split("\n")
+            ]
+
+        def assert_lines(self, lines: list[str]):
+            assert len(lines) == len(self.lines)
+            for expect, actual in zip(lines, self.lines):
+                assert actual in expect
+
+        def debug_print_lines(self):
+            print("=== DEBUG TERMINAL LINES ===")
+            for line in self.lines:
+                print(line)
+            print("=== END DEBUG TERMINAL LINES ===")
+
+    return VirtualTerminal()
